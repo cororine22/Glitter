0b98247f1491b9ca13717fa9d2d5ce08391667df
Anna Kitamoto <51523853+cororine22@users.noreply.github.com>
2020-02-09 21:17:02 +0900

    add URL form (#14)

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,4 +1,9 @@
 class TweetController < ApplicationController
   def new
   end
+
+  def create
+    @url = params[:url]
+    redirect_to("https://twitter.com/intent/tweet?text=#{@url}")
+  end
 end
