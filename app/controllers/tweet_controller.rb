2ba1eb6806aaf57304dada4f98a629ac44d6ca65
cororine22 <cororine22.prb2@gmail.com>
2020-02-11 14:57:15 +0900

    retrieve and encode URL contents

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,9 +1,23 @@
+require 'nokogiri'
+require 'pp'
+require 'open-uri'
+require 'uri'
+
 class TweetController < ApplicationController
   def new
   end
 
   def create
     @url = params[:url]
-    redirect_to("https://twitter.com/intent/tweet?text=#{@url}")
+    urlhtml = Nokogiri::HTML(open(@url))
+
+    # issueのタイトルを取得しツイート内容に表示
+    urlhtml.xpath("//h1[@class='gh-header-title f1 mr-0 flex-auto break-word']").each do |elements|
+      @text = (elements.xpath('.//span').text).gsub(/\r\n|\r|\n|\s|\t/, "")
+    end
+    query = URI.encode(@text + " " + "#Glitter")
+    @search_url = query
+
+    redirect_to("https://twitter.com/intent/tweet?text=#{@search_url}")
   end
 end
