be635f83aa3c69664f5435c1e6a57671df2d6bc8
Anna Kitamoto <51523853+cororine22@users.noreply.github.com>
2020-02-12 21:56:37 +0900

    fix URL validation (#24)

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -2,6 +2,7 @@ require 'nokogiri'
 require 'pp'
 require 'open-uri'
 require 'uri'
+require "net/http"
 
 class TweetController < ApplicationController
   def new
@@ -10,7 +11,8 @@ class TweetController < ApplicationController
   def create
     @url = "https://github.com/" + params[:url]
 
-    return until URI.regexp.match(@url)
+    # TODO: ユーザにダイアログ表示
+    return until URI.regexp.match(@url) && working_url?(@url)
 
     urlhtml = Nokogiri::HTML(open(@url))
 
@@ -18,9 +20,18 @@ class TweetController < ApplicationController
     urlhtml.xpath("//h1[@class='gh-header-title f1 mr-0 flex-auto break-word']").each do |elements|
       @text = (elements.xpath('.//span').text).gsub(/\r\n|\r|\n|\s|\t/, "")
     end
+
+    # TODO: ユーザにダイアログ表示
+    return if @text.nil?
+
     query = URI.encode(@text + " " + "#Glitter")
     @search_url = query
-
     redirect_to("https://twitter.com/intent/tweet?text=#{@search_url}")
+  end
+
+  def working_url?(url_str)
+    return open(@url).status.last == "OK"
+  rescue
+    false
   end
 end
