e92509e70ab0e4a010f73966e2b1860f95fa1fde
cororine22 <cororine22.prb2@gmail.com>
2020-02-16 21:38:38 +0900

    add meta tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
-end
+    def get_twitter_card_info(tweet)
+      twitter_card = {}
+      if tweet.present?
+        if tweet.id.present?
+          twitter_card[:url] = "https://evening-shore-25523.herokuapp.com/tweet/#{tweet.id}"
+          twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/bootcamp-glitter/images/#{tweet.id}.png"
+        else
+          twitter_card[:url] = 'https://evening-shore-25523.herokuapp.com/'
+          twitter_card[:image] = "https://raw.githubusercontent.com/ysk1180/bigtutorial/master/app/assets/images/top.png"
+        end
+      else
+        twitter_card[:url] = 'https://evening-shore-25523.herokuapp.com/'
+        twitter_card[:image] = "https://raw.githubusercontent.com/ysk1180/bigtutorial/master/app/assets/images/top.png"
+      end
+      twitter_card[:title] = "タイトル"
+      twitter_card[:card] = 'summary_large_image'
+      twitter_card[:description] = '説明文'
+      twitter_card
+    end
+  end
