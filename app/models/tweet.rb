6236512c5b4535d7c4e7d369d9bb1359d37201c4
cororine22 <cororine22.prb2@gmail.com>
2020-02-16 14:10:19 +0900

    linking S3

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,9 @@
 class Tweet < ApplicationRecord
+  after_initialize :set_default, if: :new_record?
+
+  private
+    def set_default
+        self.user ||= "default_id"
+        self.posted_at ||= Time.zone.today
+    end
 end
